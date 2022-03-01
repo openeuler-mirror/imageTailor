daa7995df6352c1f939ed00e075ccceb50010fd5
zhuchunyi <zhuchunyi@huawei.com>
2022-03-01 16:46:59 +0800

    add hook functions

---
+++ conf/common/kiwi/hook/config.sh
@@ -0,0 +1,271 @@
+:<<!
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2022. All rights reserved.
+ * imageTailor licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author:
+ * Create: 2022-02-28
+ * Description: provide image Tailor function, used as hook
+!
+
+#!/bin/bash
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+if [ -f '/usr/Euler/conf/EulerLinux.conf' ]; then
+  EL_CONFIG='/usr/Euler/conf/EulerLinux.conf'
+else
+  EL_CONFIG='/usr/Euler/conf/euler-release'
+fi
+EL_CUSTOM_RPM='/usr/custom/rpm'
+EL_CUSTOM_USRFILE='/usr/custom/usrfile'
+EL_USRRPM_FILELST='/opt/usrrpm_filelst'
+
+sys_man_cut=
+if [ -f "${EL_CONFIG}" ]; then
+  sys_man_cut=$(cat ${EL_CONFIG} | grep "^sys_man_cut=")
+  sys_man_cut=$(echo ${sys_man_cut#*=} | sed "s/^'//g" | sed "s/'$//g")
+fi
+
+if [ -z "${sys_man_cut}" ]; then
+  sys_man_cut="yes"
+fi
+
+function createUsrFileList {
+  local usrrpm_cutflag=''
+
+  ## 从EL_CONFIG 得到变量sys_man_cut ##
+  if [ -f "${EL_CONFIG}" ]; then
+    sed -i '/^#/d' ${EL_CONFIG}
+    sed -i '/^$/d' ${EL_CONFIG}
+    . ${EL_CONFIG}
+  fi
+
+  if [ -d "${EL_CUSTOM_USRFILE}" ] && [ -n "$(ls ${EL_CUSTOM_USRFILE})" ]; then
+    find ${EL_CUSTOM_USRFILE} | sed "s#${EL_CUSTOM_USRFILE}##" >>${EL_USRRPM_FILELST}
+  fi
+
+  usrrpm_cutflag=${sys_usrrpm_cut}
+  if [ "${usrrpm_cutflag}" = "yes" ]; then
+    return 0
+  fi
+
+  if [ -d "${EL_CUSTOM_RPM}" ] && [ -n "$(ls ${EL_CUSTOM_RPM}/*.rpm)" ]; then
+    find ${EL_CUSTOM_RPM} -name "*.rpm" | xargs rpm -qpl >${EL_USRRPM_FILELST}
+  fi
+
+  return 0
+}
+
+function installUserRpm() {
+  local usrrpmlog="/var/log/installUserRpm.log"
+  if [ -d "${EL_CUSTOM_RPM}" ] && [ -n "$(ls ${EL_CUSTOM_RPM}/*.rpm)" ]; then
+    rpm -Uvh --force $(find ${EL_CUSTOM_RPM} -name "*.rpm") >${usrrpmlog} 2>&1
+    if [ $? -ne 0 ]; then
+      echo "========================================================"
+      echo "ERROR: install usr rpm failed"
+      cat ${usrrpmlog}
+      echo "========================================================"
+      return 1
+    fi
+  fi
+
+  if [ -f "${usrrpmlog}" ]; then
+    chmod 640 ${usrrpmlog}
+  fi
+
+  rm -rf ${EL_CUSTOM_RPM}
+  return 0
+}
+
+function configTimeZone() {
+  local TIMEZONE=$1
+  local UTC=$2
+  local ret=0
+
+  if [ -z "${TIMEZONE}" ]; then
+    echo "TIMEZONE doesn't set."
+    ret=1
+  fi
+
+  zic -l ${TIMEZONE}
+  if [ $? != 0 ]; then
+    echo "zic -l TIMEZONE setting failed."
+    ret=1
+  fi
+  TIMEZONE=$(echo ${TIMEZONE} | sed 's#\/#\\\/#g')
+
+  if [ ! -f "/etc/sysconfig/clock" ]; then
+    echo "/etc/sysconfig/clock doesn't exist."
+    ret=1
+  else
+    sed -i "s/^TIMEZONE=.*$/TIMEZONE=\"${TIMEZONE}\"/" /etc/sysconfig/clock
+    if [ $? != 0 ]; then
+      echo "${TIMEZONE} setting failed."
+      ret=1
+    fi
+  fi
+
+  if [ "${UTC}" = "yes" ]; then
+    sed -i "s/^HWCLOCK=.*$/HWCLOCK=\"-U\"/" /etc/sysconfig/clock
+    if [ $? != 0 ]; then
+      echo "${HWCLOCK} setting failed."
+      ret=1
+    fi
+
+    hwclock --hctosys -u
+    if [ $? != 0 ]; then
+      echo "hwclock --hctosys -u setting failed."
+      ret=1
+    fi
+  else
+    sed -i "s/^HWCLOCK=.*$/HWCLOCK=\"--localtime\"/" /etc/sysconfig/clock >/dev/null 2>&1
+    if [ $? != 0 ]; then
+      echo "${HWCLOCK} setting failed."
+      ret=1
+    fi
+
+    hwclock --hctosys --localtime >/dev/null 2>&1
+    if [ $? != 0 ]; then
+      echo "hwclock --hctosys --localtime setting failed."
+      ret=1
+    fi
+  fi
+
+  return ${ret}
+}
+
+function initEulerLinux() {
+  local dl_hostname="EULER"
+  local dl_service_enable='syslog boot.localnet network sshd'
+  local dl_usermodules_autoload=''
+
+  ## obs sp1的/etc/issue.net 为UVP OS ##
+  sed -i 's/UVP OS/Euler Linux/' /etc/issue.net
+  sed -i 's/UVP OS/Euler Linux/' /etc/issue
+
+  echo "createUsrFileList start ..."
+  createUsrFileList
+
+  echo "installUserRpm start ..."
+  installUserRpm
+  if [ $? -ne 0 ]; then
+    return 1
+  fi
+
+  cp -a ${EL_CUSTOM_USRFILE}/* /
+
+  if [ -f "${EL_CONFIG}" ]; then
+    sed -i '/^#/d' "${EL_CONFIG}"
+    sed -i '/^$/d' "${EL_CONFIG}"
+    . ${EL_CONFIG}
+  fi
+
+  if [ -n "${sys_hostname}" ]; then
+    dl_hostname="${sys_hostname}"
+  fi
+  echo "${dl_hostname}" >/etc/hostname
+
+  if [ -f /etc/hosts.rpmnew ]; then
+    mv /etc/hosts.rpmnew /etc/hosts
+  fi
+
+  if [ -f /etc/hosts ]; then
+    sed -i "/\<localhost\>/!d" /etc/hosts
+    sed -i "s/ localhost / ${dl_hostname} localhost /g" /etc/hosts
+  fi
+
+  echo "configTimeZone start ..."
+  configTimeZone "${sys_timezone}" "${sys_utc}"
+
+  echo "suseRemoveAllServices start ..."
+  suseRemoveAllServices
+  if [ $? -ne 0 ]; then
+    echo "error: suseRemoveAllServices failed."
+  fi
+
+  if [ -n "${sys_service_enable}" ]; then
+    dl_service_enable="${dl_service_enable} ${sys_service_enable}"
+  fi
+
+  echo "suseInsertService start ..."
+  for i in ${dl_service_enable}; do
+    suseInsertService $i
+    if [ $? -ne 0 ]; then
+      echo "error: suseInsertService ${i} failed."
+    fi
+  done
+
+  echo "suseRemoveService start ..."
+  if [ -n "${sys_service_disable}" ]; then
+    for i in ${sys_service_disable}; do
+      suseRemoveService $i
+      if [ $? -ne 0 ]; then
+        echo "error: suseRemoveService ${i} failed."
+      fi
+    done
+  fi
+
+  if [ -n "${eulerlinux_usermodules_autoload}" ]; then
+    dl_usermodules_autoload="${eulerlinux_usermodules_autoload}"
+  fi
+
+  if [ -n "${sys_usermodules_autoload}" ]; then
+    dl_usermodules_autoload="${sys_usermodules_autoload} ${dl_usermodules_autoload}"
+  fi
+
+  for i in ${dl_usermodules_autoload}; do
+    if [ -f /etc/modules ]; then
+      grep -w "^$i" /etc/modules
+      if [ $? -ne 0 ]; then
+        echo "$i" >>/etc/modules
+      fi
+    else
+      echo "$i" >>/etc/modules
+    fi
+  done
+
+  if [ -f /etc/modules ] && [ -d /etc/modules-load.d ]; then
+    ln -s /etc/modules /etc/modules-load.d/modules.conf
+  fi
+
+  echo "getcap start ..."
+  getcap /usr/bin/* /usr/sbin/* >/root/.cap || true
+}
+
+echo "Configure image: [${kiwi_iname}]..."
+
+echo "initEulerLinux start ..."
+initEulerLinux
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+echo "suseConfig start ..."
+suseConfig
+if [ $? -ne 0 ]; then
+  echo "error: suseConfig failed."
+fi
+
+if [ "${sys_man_cut}" = "no" ]; then
+  :
+else
+  echo "baseStripDocs start ..."
+  baseStripDocs
+  if [ $? -ne 0 ]; then
+    echo "error: baseStripDocs failed."
+  fi
+fi
+
+echo "baseCleanMount start ..."
+baseCleanMount
+if [ $? -ne 0 ]; then
+  echo "error: baseCleanMount failed."
+fi
+
+exit 0
---
+++ conf/common/kiwi/hook/images.sh
@@ -0,0 +1,294 @@
+:<<!
+ * Copyright (c) Huawei Technologies Co., Ltd. 2018-2022. All rights reserved.
+ * imageTailor licensed under the Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *     http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * Author:
+ * Create: 2022-02-28
+ * Description: provide image Tailor function, used as hook
+!
+
+#!/bin/bash
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+echo "Configure image: [${kiwi_iname}]..."
+
+workdir=/opt/trans-workdir
+rpmlistdir=${workdir}/rpmlist
+rpmandsrclist=${workdir}/rpm_to_src
+srclist=${workdir}/src_list
+binlist=${workdir}/bin_list
+filelist=${workdir}/all_file
+
+rm -rf ${workdir}/*
+mkdir -p ${workdir}
+mkdir -p ${rpmlistdir}
+
+function getFileListAndSrcList() {
+  which rpm >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    return
+  fi
+
+  echo "rpm_sort_list start ..."
+  rpm_sort_list=$(rpm -qa)
+  echo "rpm_sort_list end"
+
+  for rpmn in ${rpm_sort_list}; do
+    rpm -ql ${rpmn} >${rpmlistdir}/${rpmn}
+    if [ $? -ne 0 ]; then
+      echo "get src info error: failed to get the rpm files."
+      return
+    fi
+
+    echo ${rpmn} $(rpm -q --qf "%{SOURCERPM}" ${rpmn} | sed 's/.src.rpm//g') >>${rpmandsrclist}
+    if [ $? -ne 0 ]; then
+      echo "get src info error: failed to get rpm and src."
+      return
+    fi
+  done
+}
+
+function findAllFiles() {
+  find / -type f | grep -v '^/proc' | grep -v '/etc/ima/digest_lists' >${filelist}
+  if [ $? -ne 0 ]; then
+    echo "get src info error: findAllFiles failed."
+    return 1
+  fi
+
+  for dir in /usr/bin/ /usr/sbin/ /usr/lib/ /usr/lib64/; do
+    grep ^${dir} ${filelist} | sed 's:^/usr/:/:g' >>${filelist}
+    if [ $? -ne 0 ]; then
+      echo "error: sed filelist failed: ${dir}"
+      return 1
+    fi
+  done
+
+  if [ ! -s ${filelist} ]; then
+    return 1
+  fi
+}
+
+function getAllFilesAndPrint() {
+  findAllFiles
+  if [ $? -ne 0 ]; then
+    echo "get src info error: failed to find all files."
+    return 1
+  fi
+
+  echo "start print initrd file list"
+  cat ${filelist}
+  echo "end print initrd file list"
+}
+
+function getSrcInfo() {
+  rpm --test >/dev/null 2>&1
+  if [ $? -eq 0 -a "${sys_cut}" = "no" ]; then
+    getSrcInfoFromRpm
+    return
+  fi
+
+  if [ ! -f "${rpmandsrclist}" ]; then
+    getAllFilesAndPrint
+    return
+  fi
+
+  findAllFiles
+  if [ $? -ne 0 ]; then
+    echo "get src info error: failed to find all files."
+    return 1
+  fi
+
+  while read line; do
+    find_rpm=$(grep ^${line}$ ${rpmlistdir}/* | awk -F ':' '{print $1}')
+    if [ -n "${find_rpm}" ]; then
+      for rname in ${find_rpm}; do
+        grep $(basename $rname) ${rpmandsrclist} | awk '{print $2}' >>${workdir}/find_src
+        if [ $? -ne 0 ]; then
+          echo "get src info error: failed to grep ${rname} ${rpmandsrclist}."
+          return 1
+        fi
+
+        grep $(basename $rname) ${rpmandsrclist} | awk '{print $1}' >>${workdir}/find_info
+        if [ $? -ne 0 ]; then
+          echo "get bin info error: failed to grep ${rname} ${rpmandsrclist}."
+          return 1
+        fi
+      done
+    else
+      echo ${line} >>${workdir}/not_grep_files.log
+    fi
+  done <${filelist}
+
+  cat ${workdir}/find_src | sort | uniq >${srclist}
+  catSrcInfoToLog
+
+  cat ${workdir}/find_info | sort | uniq >${binlist}
+  catBinInfoToLog
+}
+
+function catBinInfoToLog() {
+  echo "start print bin info"
+  cat ${binlist}
+  echo "end print bin info"
+}
+
+function catSrcInfoToLog() {
+  echo "start print src info"
+  cat ${srclist}
+  echo "end print src info"
+}
+
+function getSrcInfoFromRpm() {
+  rpm -qa --qf "%{SOURCERPM}\n" | sed 's/.src.rpm//g' | uniq >${srclist}
+  if [ $? -ne 0 ]; then
+    echo "get src info error: getSrcInfoFromRpm failed."
+    return
+  fi
+
+  catSrcInfoToLog
+
+  rpm -qa | uniq >${binlist}
+  if [ $? -ne 0 ]; then
+    echo "get bin info error: getBinInfoFromRpm failed."
+    return
+  fi
+
+  catBinInfoToLog
+}
+
+EL_CONFIG='/usr/Euler/conf/euler-release'
+if [ -f '/usr/Euler/conf/EulerLinux.conf' ]; then
+  EL_CONFIG='/usr/Euler/conf/EulerLinux.conf'
+fi
+
+EL_USRRPM_FILELIST='/opt/usrrpm_filelst'
+EL_UER_KOLST='/opt/usrrpm_filelst_kolst'
+
+sys_cut=
+sys_language=
+sys_gconv=
+sys_translations=
+if [ -f ${EL_CONFIG} ]; then
+  sys_cut=$(cat $EL_CONFIG | grep "^sys_cut=")
+  sys_cut=$(echo ${sys_cut#*=} | sed "s/^'//g" | sed "s/'$//g")
+
+  sys_language=$(cat $EL_CONFIG | grep "^sys_language=")
+  sys_language=$(echo ${sys_language#*=} | sed "s/^'//g" | sed "s/'$//g")
+
+  sys_gconv=$(cat $EL_CONFIG | grep "^sys_gconv=")
+  sys_gconv=$(echo ${sys_gconv#*=} | sed "s/^'//g" | sed "s/'$//g")
+
+  sys_translations=$(cat $EL_CONFIG | grep "^sys_translations=")
+  sys_translations=$(echo ${sys_translations#*=} | sed "s/^'//g" | sed "s/'$//g")
+fi
+
+#======================================
+# default sys_cut=yes
+#======================================
+if [ -z "${sys_cut}" ]; then
+  sys_cut="yes"
+fi
+
+if [ -f '/usr/Euler/conf/EulerLinux.conf' ]; then
+  rm -rf /etc/sysconfig/grub
+fi
+
+if [ "${sys_cut}" = "yes" ] || [ "${sys_cut}" = "debug" ]; then
+  echo "getFileListAndSrcList start ..."
+  getFileListAndSrcList
+  echo "getFileListAndSrcList end"
+
+  #================================================
+  # add usr ko to kiwi_drivers, will keep in system
+  #================================================
+  echo "read line start ..."
+  if [ -f ${EL_USRRPM_FILELIST} ]; then
+    grep ^/lib/modules/ ${EL_USRRPM_FILELIST} | grep kernel | grep ko$ >$EL_UER_KOLST
+    while read line; do
+      koname=${line#/lib/modules/}
+      koname=${koname#*/kernel/}
+      kiwi_drivers="${kiwi_drivers},${koname}"
+    done <$EL_UER_KOLST
+  fi
+  echo "read line end"
+
+  #================================================
+  # remove unneeded kernel files
+  #================================================
+  echo "suseStripKernel start ..."
+  suseStripKernel
+  echo "suseStripKernel end"
+
+  #================================================
+  # remove unneeded initrd files
+  #================================================
+  echo "suseStripInitrd start ..."
+  suseStripInitrd
+  echo "suseStripInitrd end"
+
+  echo "baseStripLocales start ..."
+  if [ -z "${sys_language}" ] || [ "${sys_language}" = "null" ] || [ "${sys_language}" = "NULL" ]; then
+    baseStripLocales $(for i in $(echo ${kiwi_language} | tr "," " "); do echo -n "${i}.utf8 "; done)
+  elif [ "${sys_language}" != "all" ] && [ "${sys_language}" = "ALL" ]; then
+    baseStripLocales $(for i in $(echo ${sys_language} | tr "," " "); do echo -n "${i} "; done)
+  fi
+  echo "baseStripLocales end"
+
+  echo "baseStripTranslations start ..."
+  if [ -z "${sys_translations}" ] || [ "${sys_translations}" = "null" ] || [ "${sys_translations}" = "NULL" ]; then
+    baseStripTranslations kiwi.mo
+  elif [ "${sys_translations}" != "all" ] && [ "${sys_translations}" != "ALL" ]; then
+    baseStripTranslations $(for i in $(echo ${sys_translations} | tr "," " "); do echo -n "${i} "; done)
+  fi
+  echo "baseStripTranslations end"
+
+  echo "baseStripGconv start ..."
+  if [ -z "${sys_gconv}" ] || [ "${sys_gconv}" = "null" ] || [ "${sys_gconv}" = "NULL" ]; then
+    directories="/usr/lib/gconv /usr/lib64/gconv"
+    find ${directories} 2>/dev/null | xargs rm -f
+    if [ $? -ne 0 ]; then
+      echo "delete directories failed: ${directories}"
+    fi
+  elif [ "${sys_gconv}" != "all" ] && [ "${sys_gconv}" != "ALL" ]; then
+    baseStripGconv "$(for i in $(echo ${sys_gconv} | tr "," " "); do echo -n "${i} "; done) gconv-modules gconv-modules.cache"
+  fi
+  echo "baseStripGconv end"
+
+fi
+
+echo "getSrcInfo start ..."
+getSrcInfo
+echo "getSrcInfo end"
+
+cp -a /usr/custom/usrfile/* /
+depmod -a $(uname -r)
+
+if [ -f /etc/modules ] && [ -d /usr/Euler/conf ]; then
+  cat /etc/modules >>/usr/Euler/conf/modules
+fi
+
+rm -rf /usr/custom
+chmod +x /etc/rc.d/rc.local 2>/dev/null
+
+if [ -f '/usr/Euler/conf/EulerLinux.conf' ]; then
+  [ -d /usr/lib/grub ] && ln -s /usr/lib/grub /usr/lib/grub2
+fi
+
+#================================================
+# umount /proc
+#================================================
+echo "umount start ..."
+umount /proc &>/dev/null
+rm -rf /.profile /.kconfig /image /opt/need_rpmlst &>/dev/null
+rm -rf ${EL_USRRPM_FILELIST} ${EL_UER_KOLST}
+rm -rf /etc/euler-release
+rm -rf ${workdir}
+echo "umount end"
+
+exit 0
